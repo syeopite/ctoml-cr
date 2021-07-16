bb8f85bb9159c59e03dbc3d6a2829309f358a866
syeopite <syeopite@syeopite.dev>
2021-07-15 22:19:43 -0700

    Add methods similar to JSON::Any for CtomlCr::Any

--- src/any.cr
+++ src/any.cr
@@ -15,4 +15,257 @@ struct CtomlCr::Any
   # raises when the underlying value is not an `Array` or `Hash`
   def size : Int32
   end
+
+  # Assumes the underlying value is an `Array` and returns the element
+  # at the given index.
+  # Raises if the underlying value is not an `Array`.
+  def [](index : Int) : CtomlCr::Any
+    case object = @raw
+    when Array
+      object[index]
+    else
+      raise "Expected Array for #[](index : Int), not #{object.class}"
+    end
+  end
+
+  # Assumes the underlying value is an `Array` and returns the element
+  # at the given index, or `nil` if out of bounds.
+  # Raises if the underlying value is not an `Array`.
+  def []?(index : Int) : CtomlCr::Any?
+    case object = @raw
+    when Array
+      object[index]?
+    else
+      raise "Expected Array for #[]?(index : Int), not #{object.class}"
+    end
+  end
+
+  # Assumes the underlying value is a `Hash` and returns the element
+  # with the given key.
+  # Raises if the underlying value is not a `Hash`.
+  def [](key : String) : CtomlCr::Any
+    case object = @raw
+    when Hash
+      object[key]
+    else
+      raise "Expected Hash for #[](key : String), not #{object.class}"
+    end
+  end
+
+  # Assumes the underlying value is a `Hash` and returns the element
+  # with the given key, or `nil` if the key is not present.
+  # Raises if the underlying value is not a `Hash`.
+  def []?(key : String) : CtomlCr::Any?
+    case object = @raw
+    when Hash
+      object[key]?
+    else
+      raise "Expected Hash for #[]?(key : String), not #{object.class}"
+    end
+  end
+
+  # Traverses the depth of a structure and returns the value.
+  # Returns `nil` if not found.
+  def dig?(index_or_key : String | Int, *subkeys) : CtomlCr::Any?
+    self[index_or_key]?.try &.dig?(*subkeys)
+  end
+
+  # :nodoc:
+  def dig?(index_or_key : String | Int) : CtomlCr::Any?
+    case @raw
+    when Hash, Array
+      self[index_or_key]?
+    else
+      nil
+    end
+  end
+
+  # Traverses the depth of a structure and returns the value, otherwise raises.
+  def dig(index_or_key : String | Int, *subkeys) : CtomlCr::Any
+    self[index_or_key].dig(*subkeys)
+  end
+
+  # :nodoc:
+  def dig(index_or_key : String | Int) : CtomlCr::Any
+    self[index_or_key]
+  end
+
+  # Checks that the underlying value is `Nil`, and returns `nil`.
+  # Raises otherwise.
+  def as_nil : Nil
+    @raw.as(Nil)
+  end
+
+  # Checks that the underlying value is `Bool`, and returns its value.
+  # Raises otherwise.
+  def as_bool : Bool
+    @raw.as(Bool)
+  end
+
+  # Checks that the underlying value is `Bool`, and returns its value.
+  # Returns `nil` otherwise.
+  def as_bool? : Bool?
+    as_bool if @raw.is_a?(Bool)
+  end
+
+  # Checks that the underlying value is `Int`, and returns its value as an `Int32`.
+  # Raises otherwise.
+  def as_i : Int32
+    @raw.as(Int).to_i
+  end
+
+  # Checks that the underlying value is `Int`, and returns its value as an `Int32`.
+  # Returns `nil` otherwise.
+  def as_i? : Int32?
+    as_i if @raw.is_a?(Int)
+  end
+
+  # Checks that the underlying value is `Int`, and returns its value as an `Int64`.
+  # Raises otherwise.
+  def as_i64 : Int64
+    @raw.as(Int).to_i64
+  end
+
+  # Checks that the underlying value is `Int`, and returns its value as an `Int64`.
+  # Returns `nil` otherwise.
+  def as_i64? : Int64?
+    as_i64 if @raw.is_a?(Int64)
+  end
+
+  # Checks that the underlying value is `Float`, and returns its value as an `Float64`.
+  # Raises otherwise.
+  def as_f : Float64
+    @raw.as(Float64)
+  end
+
+  # Checks that the underlying value is `Float`, and returns its value as an `Float64`.
+  # Returns `nil` otherwise.
+  def as_f? : Float64?
+    @raw.as?(Float64)
+  end
+
+  # Checks that the underlying value is `Float`, and returns its value as an `Float32`.
+  # Raises otherwise.
+  def as_f32 : Float32
+    @raw.as(Float).to_f32
+  end
+
+  # Checks that the underlying value is `Float`, and returns its value as an `Float32`.
+  # Returns `nil` otherwise.
+  def as_f32? : Float32?
+    as_f32 if @raw.is_a?(Float)
+  end
+
+  # Checks that the underlying value is `String`, and returns its value.
+  # Raises otherwise.
+  def as_s : String
+    @raw.as(String)
+  end
+
+  # Checks that the underlying value is `String`, and returns its value.
+  # Returns `nil` otherwise.
+  def as_s? : String?
+    as_s if @raw.is_a?(String)
+  end
+
+  # Checks that the underlying value is `Array`, and returns its value.
+  # Raises otherwise.
+  def as_a : Array(Any)
+    @raw.as(Array)
+  end
+
+  # Checks that the underlying value is `Array`, and returns its value.
+  # Returns `nil` otherwise.
+  def as_a? : Array(Any)?
+    as_a if @raw.is_a?(Array)
+  end
+
+  # Checks that the underlying value is `Hash`, and returns its value.
+  # Raises otherwise.
+  def as_h : Hash(String, Any)
+    @raw.as(Hash)
+  end
+
+  # Checks that the underlying value is `Hash`, and returns its value.
+  # Returns `nil` otherwise.
+  def as_h? : Hash(String, Any)?
+    as_h if @raw.is_a?(Hash)
+  end
+
+  # :nodoc:
+  def inspect(io : IO) : Nil
+    @raw.inspect(io)
+  end
+
+  # :nodoc:
+  def to_s(io : IO) : Nil
+    @raw.to_s(io)
+  end
+
+  # :nodoc:
+  def pretty_print(pp)
+    @raw.pretty_print(pp)
+  end
+
+  # Returns `true` if both `self` and *other*'s raw object are equal.
+  def ==(other : CtomlCr::Any)
+    raw == other.raw
+  end
+
+  # Returns `true` if the raw object is equal to *other*.
+  def ==(other)
+    raw == other
+  end
+
+  # See `Object#hash(hasher)`
+  def_hash raw
+
+  # :nodoc:
+  def to_json(json : JSON::Builder)
+    raw.to_json(json)
+  end
+
+  def to_yaml(yaml : YAML::Nodes::Builder)
+    raw.to_yaml(yaml)
+  end
+
+  # Returns a new CtomlCr::Any instance with the `raw` value `dup`ed.
+  def dup
+    Any.new(raw.dup)
+  end
+
+  # Returns a new CtomlCr::Any instance with the `raw` value `clone`ed.
+  def clone
+    Any.new(raw.clone)
+  end
+end
+
+class Object
+  def ===(other : CtomlCr::Any)
+    self === other.raw
+  end
+end
+
+struct Value
+  def ==(other : CtomlCr::Any)
+    self == other.raw
+  end
+end
+
+class Reference
+  def ==(other : CtomlCr::Any)
+    self == other.raw
+  end
+end
+
+class Array
+  def ==(other : CtomlCr::Any)
+    self == other.raw
+  end
+end
+
+class Hash
+  def ==(other : CtomlCr::Any)
+    self == other.raw
+  end
 end
