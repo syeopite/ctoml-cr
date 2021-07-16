fc5e86ea6d94219244a07110ea0ba2baa9a5d9dc
syeopite <syeopite@syeopite.dev>
2021-07-15 23:18:46 -0700

    Add basic specs

--- spec/ctoml-cr_spec.cr
+++ spec/ctoml-cr_spec.cr
@@ -1,9 +1,25 @@
 require "./spec_helper"
 
-describe Ctoml::Cr do
-  # TODO: Write tests
+describe TOML do
+  it "#parse" do
+    data = File.read("spec/test.toml")
+    toml = TOML.parse(data)
 
-  it "works" do
-    false.should eq(true)
+    toml["number"].should eq(123)
+    toml["floats"].should eq(123.123)
+    toml["array"].should eq([1, 2, 3, 4, 5, 6])
+
+    typeof(toml["number"].as_i).should eq(Int32)
+    typeof(toml["number"].as_i64).should eq(Int64)
+
+    typeof(toml["floats"].as_f).should eq(Float64)
+    typeof(toml["floats"].as_f32).should eq(Float32)
+
+    typeof(toml["arrays"]).should eq(TOML::Any)
+    typeof(toml["arrays"].as_a).should eq(Array(TOML::Any))
+
+    toml["nested_arraies"].should eq([[1, 2, 3, 4, 5, 6, [7, 8, 9, 10, 11]], "Hello", "testing"])
+    toml["table-1"].should eq({"key" => "something", "key2" => "Another key", "inline-table" => {"first" => "Tom", "last" => "Preston-Werner"}})
+    toml["products"].should eq([{"name" => "Hammer", "sku" => 738594937}, Hash(String, TOML::Any).new, {"name" => "Nail", "sku" => 284758393, "color" => "gray"}])
   end
 end
