a8689dfb4f62384a3a88d752cc1ff48b42a90dd5
syeopite <syeopite@syeopite.dev>
2021-07-15 22:30:40 -0700

    Change TOML struct to module and move any to it

--- src/any.cr
+++ src/any.cr
@@ -1,7 +1,8 @@
-# CtomlCr::Any is a convenient wrapper around all possible TOML types (CtomlCr::Any::Type) and can be used for traversing dynamic or unknown TOML structures.
+# TOML::Any is a convenient wrapper around all possible TOML types (TOML::Any::Type) and can be used for traversing dynamic or unknown TOML structures.
 #
 # Aka it's the same as what `JSON::Any`, and `YAML::Any` does but for TOML.
-struct CtomlCr::Any
+struct TOML::Any
+  # All possible TOML types
   alias Type = String | Int64 | Float64 | Bool | Time | Hash(String, Any) | Array(Any)
 
   # Returns the raw underlying value.
@@ -19,7 +20,7 @@ struct CtomlCr::Any
   # Assumes the underlying value is an `Array` and returns the element
   # at the given index.
   # Raises if the underlying value is not an `Array`.
-  def [](index : Int) : CtomlCr::Any
+  def [](index : Int) : TOML::Any
     case object = @raw
     when Array
       object[index]
@@ -31,7 +32,7 @@ struct CtomlCr::Any
   # Assumes the underlying value is an `Array` and returns the element
   # at the given index, or `nil` if out of bounds.
   # Raises if the underlying value is not an `Array`.
-  def []?(index : Int) : CtomlCr::Any?
+  def []?(index : Int) : TOML::Any?
     case object = @raw
     when Array
       object[index]?
@@ -43,7 +44,7 @@ struct CtomlCr::Any
   # Assumes the underlying value is a `Hash` and returns the element
   # with the given key.
   # Raises if the underlying value is not a `Hash`.
-  def [](key : String) : CtomlCr::Any
+  def [](key : String) : TOML::Any
     case object = @raw
     when Hash
       object[key]
@@ -55,7 +56,7 @@ struct CtomlCr::Any
   # Assumes the underlying value is a `Hash` and returns the element
   # with the given key, or `nil` if the key is not present.
   # Raises if the underlying value is not a `Hash`.
-  def []?(key : String) : CtomlCr::Any?
+  def []?(key : String) : TOML::Any?
     case object = @raw
     when Hash
       object[key]?
@@ -66,12 +67,12 @@ struct CtomlCr::Any
 
   # Traverses the depth of a structure and returns the value.
   # Returns `nil` if not found.
-  def dig?(index_or_key : String | Int, *subkeys) : CtomlCr::Any?
+  def dig?(index_or_key : String | Int, *subkeys) : TOML::Any?
     self[index_or_key]?.try &.dig?(*subkeys)
   end
 
   # :nodoc:
-  def dig?(index_or_key : String | Int) : CtomlCr::Any?
+  def dig?(index_or_key : String | Int) : TOML::Any?
     case @raw
     when Hash, Array
       self[index_or_key]?
@@ -81,12 +82,12 @@ struct CtomlCr::Any
   end
 
   # Traverses the depth of a structure and returns the value, otherwise raises.
-  def dig(index_or_key : String | Int, *subkeys) : CtomlCr::Any
+  def dig(index_or_key : String | Int, *subkeys) : TOML::Any
     self[index_or_key].dig(*subkeys)
   end
 
   # :nodoc:
-  def dig(index_or_key : String | Int) : CtomlCr::Any
+  def dig(index_or_key : String | Int) : TOML::Any
     self[index_or_key]
   end
 
@@ -208,7 +209,7 @@ struct CtomlCr::Any
   end
 
   # Returns `true` if both `self` and *other*'s raw object are equal.
-  def ==(other : CtomlCr::Any)
+  def ==(other : TOML::Any)
     raw == other.raw
   end
 
@@ -229,12 +230,12 @@ struct CtomlCr::Any
     raw.to_yaml(yaml)
   end
 
-  # Returns a new CtomlCr::Any instance with the `raw` value `dup`ed.
+  # Returns a new TOML::Any instance with the `raw` value `dup`ed.
   def dup
     Any.new(raw.dup)
   end
 
-  # Returns a new CtomlCr::Any instance with the `raw` value `clone`ed.
+  # Returns a new TOML::Any instance with the `raw` value `clone`ed.
   def clone
     Any.new(raw.clone)
   end
@@ -242,35 +243,35 @@ end
 
 # :nodoc:
 class Object
-  def ===(other : CtomlCr::Any)
+  def ===(other : TOML::Any)
     self === other.raw
   end
 end
 
 # :nodoc:
 struct Value
-  def ==(other : CtomlCr::Any)
+  def ==(other : TOML::Any)
     self == other.raw
   end
 end
 
 # :nodoc:
 class Reference
-  def ==(other : CtomlCr::Any)
+  def ==(other : TOML::Any)
     self == other.raw
   end
 end
 
 # :nodoc:
 class Array
-  def ==(other : CtomlCr::Any)
+  def ==(other : TOML::Any)
     self == other.raw
   end
 end
 
 # :nodoc:
 class Hash
-  def ==(other : CtomlCr::Any)
+  def ==(other : TOML::Any)
     self == other.raw
   end
 end
--- src/ctoml-cr.cr
+++ src/ctoml-cr.cr
@@ -2,96 +2,92 @@ require "./libtoml"
 require "./helpers"
 require "./any"
 
-# TODO: Write documentation for `Ctoml::Cr`
-module CtomlCr
-  VERSION = "0.1.0"
 
-  struct TOML
-    def load(file)
-      raw_string = File.read(file)
-      contents = LibToml.toml_parse(raw_string, out error, 200)
-      if !contents
-        raise CTomlCrExceptions::TomlParseError.new("Error in file: '#{file}' that prevents parsing.")
-      end
-
-      return CtomlCr::Any.new(self.fetch_table(contents))
+module TOML
+  def self.load(file)
+    raw_string = File.read(file)
+    contents = LibToml.toml_parse(raw_string, out error, 200)
+    if !contents
+      raise CTomlCrExceptions::TomlParseError.new("Error in file: '#{file}' that prevents parsing.")
     end
 
-    private def fetch_table(contents)
-      table = {} of String => CtomlCr::Any
+    return TOML::Any.new(self.fetch_table(contents))
+  end
 
-      contents.value.nkval.times do |i|
-        item =  contents.value.kval[i]
-        key = String.new(item.value.key)
+  private def self.fetch_table(contents)
+    table = {} of String => TOML::Any
 
-        table[key] = CtomlCr::Any.new(fetch_value(item))
-      end
+    contents.value.nkval.times do |i|
+      item =  contents.value.kval[i]
+      key = String.new(item.value.key)
 
-      contents.value.narr.times do |i|
-        array_to_parse = contents.value.arr[i]
-        key = String.new(array_to_parse.value.key)
-
-        arr = CtomlCr::Any.new(fetch_array(array_to_parse))
-      end
-
-      contents.value.ntab.times do |i|
-        table_to_parse = contents.value.tab[i]
-        key = String.new(table_to_parse.value.key)
-
-        table[key] = CtomlCr::Any.new(fetch_table(table_to_parse))
-      end
-
-      LibC.free(contents)
-
-      return table
+      table[key] = TOML::Any.new(fetch_value(item))
     end
 
-    private def fetch_value(item)
-      # Parse key-values, if any
-      value = parse_underlying(item.value.val)
+    contents.value.narr.times do |i|
+      array_to_parse = contents.value.arr[i]
+      key = String.new(array_to_parse.value.key)
 
-      LibC.free(item.value.key)
-      LibC.free(item.value.val)
-      LibC.free(item)
-
-      return value
+      arr = TOML::Any.new(fetch_array(array_to_parse))
     end
 
-    private def fetch_array(array_to_parse)
-      items = [] of CtomlCr::Any
-      array_to_parse.value.nitem.times do |array_index|
-        item : LibToml::TomlArritemT = array_to_parse.value.item[array_index]
+    contents.value.ntab.times do |i|
+      table_to_parse = contents.value.tab[i]
+      key = String.new(table_to_parse.value.key)
 
-        # Each TomlArritemT can only store one of the following:
-        if item.arr
-          items << CtomlCr::Any.new(fetch_array(item.arr))
-          LibC.free(item.arr)
-        elsif item.tab
-          items << CtomlCr::Any.new(fetch_table(item.tab))
-          LibC.free(item.tab)
-        else
-          items << CtomlCr::Any.new(parse_underlying(item.val))
-          LibC.free(item.val)
-        end
-      end
-
-      LibC.free(array_to_parse)
-      return items
+      table[key] = TOML::Any.new(fetch_table(table_to_parse))
     end
 
-    private def parse_underlying(raw)
-      raw = String.new(raw)
-      if raw.starts_with? '"'
-        value = raw.strip('"')
+    LibC.free(contents)
+
+    return table
+  end
+
+  private def self.fetch_value(item)
+    # Parse key-values, if any
+    value = parse_underlying(item.value.val)
+
+    LibC.free(item.value.key)
+    LibC.free(item.value.val)
+    LibC.free(item)
+
+    return value
+  end
+
+  private def self.fetch_array(array_to_parse)
+    items = [] of TOML::Any
+    array_to_parse.value.nitem.times do |array_index|
+      item : LibToml::TomlArritemT = array_to_parse.value.item[array_index]
+
+      # Each TomlArritemT can only store one of the following:
+      if item.arr
+        items << TOML::Any.new(fetch_array(item.arr))
+        LibC.free(item.arr)
+      elsif item.tab
+        items << TOML::Any.new(fetch_table(item.tab))
+        LibC.free(item.tab)
       else
-        begin
-          value = Time::Format::RFC_3339.parse(raw)
-        rescue ex : Time::Format::Error
-          value = raw.to_i64
-        end
+        items << TOML::Any.new(parse_underlying(item.val))
+        LibC.free(item.val)
       end
-
-      return value
     end
+
+    LibC.free(array_to_parse)
+    return items
+  end
+
+  private def self.parse_underlying(raw)
+    raw = String.new(raw)
+    if raw.starts_with? '"'
+      value = raw.strip('"')
+    else
+      begin
+        value = Time::Format::RFC_3339.parse(raw)
+      rescue ex : Time::Format::Error
+        value = raw.to_i64
+      end
+    end
+
+    return value
   end
 end
